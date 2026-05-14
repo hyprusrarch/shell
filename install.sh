325a44150a48e8953961069a1d14169c7b972c9b
hyprusrarch <amongus878789@gmail.com>
2026-05-14 08:38:32 +0700

    fix install script agin

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ if confirm "Do you want to proceed with the installation?"; then
         echo "yay not found. Installing yay..."
         sudo pacman -S --needed --noconfirm git base-devel
         git clone https://aur.archlinux.org/yay.git
-        cd yay
+        cd yay || exit
         makepkg -si --noconfirm
         cd ..
         rm -rf yay
@@ -26,21 +26,27 @@ if confirm "Do you want to proceed with the installation?"; then
     sudo pacman -S --needed --noconfirm quickshell hypridle hyprlock foot cava \
     cowsay fortune-mod papirus-icon-theme fastfetch nautilus mpv \
     gpu-screen-recorder hyprshot wtype hyprland flatpak \
-    hyprpolkitagent wl-clipboard cliphist cloudflare-warp-bin btop ttf-cascadia-code-nerd noto-fonts-emoji noto-fonts ttf-jetbrains-mono-nerd ttf-jetbrains-mono nwg-look awww
+    hyprpolkitagent wl-clipboard cliphist btop ttf-cascadia-code-nerd noto-fonts-emoji noto-fonts ttf-jetbrains-mono-nerd ttf-jetbrains-mono nwg-look awww cloudflare-warp-bin
 
     yay -S --noconfirm matugen-bin
 
     sudo pacman -Rns --noconfirm gnu-free-fonts
 
-    mv $HOME/.config youroldconfig
-    mv $HOME/shell/.config/ $HOME/.config
-    mv $HOME/shell/.scripts $HOME
-    mv $HOME/shell/.wallpapers $HOME
-    mv $HOME/.bashrc $HOME/.bashrc.bak
-    mv $HOME/shell/assets/.bashrc $HOME
-    sudo cp -r $HOME/shell/assets/gojo.cow /usr/share/cowsay/cows
-    awww daemon
-    awww img $HOME/shell/assets/chick.jpg
+
+    mv "$HOME/.config" "$HOME/youroldconfig"
+    cp -r "$HOME/shell/.config/" "$HOME/.config"
+    cp -r "$HOME/shell/.scripts" "$HOME"
+    cp -r "$HOME/shell/.wallpapers" "$HOME"
+    mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
+    cp "$HOME/shell/assets/.bashrc" "$HOME"
+
+    sudo mkdir -p /usr/share/cowsay/cows
+    sudo cp "$HOME/shell/assets/gojo.cow" /usr/share/cowsay/cows/
+
+    awww daemon &
+    sleep 1
+    awww img "$HOME/shell/assets/chick.jpg"
+
     echo "Installation completed, youroldconfig is your old config ;-;, and your old bash will be bashrc.bak"
 else
     echo "cancelled by user"
